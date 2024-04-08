f1a46da9ad4e5701a5fe15b5854618faf44d4d47
TLS\Sowjanya.Katta <sowjanya.katta@taslog.com.au>
2024-04-08 14:57:03 +1000

    Registration

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import RegisterForm from '../components/auth/RegisterForm'
 
 const RegisterScreen = () => {
   return (
     <View>
       <Text>RegisterScreen</Text>
+      {/* <RegisterForm></RegisterForm> */}
     </View>
   )
 }
