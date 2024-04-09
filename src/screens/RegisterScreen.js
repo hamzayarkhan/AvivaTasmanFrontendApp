b3b822726bacb74b48b96ac6ba93ebbfebcec2e4
TLS\Sowjanya.Katta <sowjanya.katta@taslog.com.au>
2024-04-09 11:39:57 +1000

    Register

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,14 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import RegisterForm from '../components/auth/RegisterForm'
 
+
 const RegisterScreen = () => {
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-      {/* <RegisterForm></RegisterForm> */}
+  
+     <View style= {styles.container}>
+      <View style= {styles.imageContainer}>
+      <Image
+                    source={require("../../assets/images/logo.png")} // Replace with your logo URL
+                    style={styles.logo}
+                />
+ 
+      </View>
+            <RegisterForm></RegisterForm> 
     </View>
+  
+   
+   
   )
 }
-
+const styles = StyleSheet.create({
+  container: { 
+    justifyContent: 'center',
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#ffffff'
+  },
+  imageContainer: { 
+    justifyContent: 'center',
+    flexDirection:'row'
+  },
+  
+  logo: {
+      // Adjust these values according to your logo's size
+      width: 130,
+      height: 130,
+      resizeMode: 'contain',
+      a:"center"// This ensures your logo scales properly
+  },
+});
 export default RegisterScreen
